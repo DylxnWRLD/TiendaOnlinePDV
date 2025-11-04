bb07965cf097f18e9f8882b41e90a44fd61b445e
JoseManuUkyo <manuel19842023@outlook.com>
2025-11-03 23:12:40 -0600

    imagen subida desde el supabase

---
+++ frontend/compraCliente/compra.js
@@ -0,0 +1,16 @@
+const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://127.0.0.1:3000'
+    : 'https://tiendaonlinepdv-hm20.onrender.com'; // ⭐️ Revisa esta URL para Render ⭐️
+
+// Obtención de datos de sesión del localStorage
+const token = localStorage.getItem('supabase-token'); 
+const corteId = localStorage.getItem('currentCorteId');
+const role = localStorage.getItem('user-role'); 
+
+// Estado local de la venta (el "carrito")
+let ventaActual = {
+    productos: [], // Contiene {id_producto_mongo, nombre_producto, precio_unitario, cantidad, monto_descuento, stock_disponible}
+    subtotal: 0,
+    descuento: 0,
+    total: 0
+};
