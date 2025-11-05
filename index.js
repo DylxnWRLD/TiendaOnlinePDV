89cde8a5d2ed21da1e2173863a0de86bbdaab7f1
Alvar-234 <thealvar234@gmail.com>
2025-11-05 09:12:23 -0600

    Inicio sesion cliente correcion

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const loginBtn = document.getElementById("loginBtn");
 const cartBtn = document.getElementById("cartBtn");
 const menuToggle = document.getElementById("menuToggle");
 
+// Obtención de datos de sesión del localStorage
+const token = localStorage.getItem('supabase-token');
+const corteId = localStorage.getItem('currentCorteId');
+const role = localStorage.getItem('user-role');
+
 if (token && role) {
   // --- Usuario LOGUEADO ---
 
@@ -119,11 +124,6 @@ document.querySelectorAll(".product-card a").forEach(card => {
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://127.0.0.1:3000'
   : 'https://tiendaonlinepdv-hm20.onrender.com'; // ⭐️ Revisa esta URL para Render ⭐️
-
-// Obtención de datos de sesión del localStorage
-const token = localStorage.getItem('supabase-token');
-const corteId = localStorage.getItem('currentCorteId');
-const role = localStorage.getItem('user-role');
 
 // Estado local de la venta (el "carrito")
 let ventaActual = {
